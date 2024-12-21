9d5a767fce8e127bcb6d5acae0424844e7425d62
Kevin J. Lynagh <kevin@keminglabs.com>
2024-12-21 18:41:34 +0100

    Format.

--- whispertron/AppDelegate.swift
+++ whispertron/AppDelegate.swift
@@ -1,179 +1,185 @@
-import Cocoa
-import SwiftUI
-import Foundation
-import os
 import AVFoundation
-import HotKey
 import ApplicationServices
+import Cocoa
 import CoreGraphics
+import Foundation
+import HotKey
+import SwiftUI
+import os
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-    private var statusItem: NSStatusItem!
-    private var whisperContext: WhisperContext?
-    private var recorder: Recorder?
-    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "AppDelegate")
-    private let standbyImage = NSImage(systemSymbolName: "circle", accessibilityDescription: "Standby")
-    private let recordingImage = NSImage(systemSymbolName: "circle.fill", accessibilityDescription: "Recording")
-    private var hotKey: HotKey?
+  private var statusItem: NSStatusItem!
+  private var whisperContext: WhisperContext?
+  private var recorder: Recorder?
+  private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "AppDelegate")
+  private let standbyImage = NSImage(
+    systemSymbolName: "circle", accessibilityDescription: "Standby")
+  private let recordingImage = NSImage(
+    systemSymbolName: "circle.fill", accessibilityDescription: "Recording")
+  private var hotKey: HotKey?
 
-    private var feedbackWindow: NSWindow?
-    private var feedbackTextField: NSTextField?
-    private var lastTranscript = ""
-    private let MinimumTranscriptionDuration = 1.0
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
+  private var feedbackWindow: NSWindow?
+  private var feedbackTextField: NSTextField?
+  private var lastTranscript = ""
+  private let MinimumTranscriptionDuration = 1.0
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
 
-        self.hotKey = HotKey(key: .h, modifiers: [.control, .shift])
-        hotKey?.keyDownHandler = { [weak self] in
-            self?.didTapRecording()
-        }
-        hotKey?.keyUpHandler = { [weak self] in
-            self?.didTapStandby()
-        }
-
-        setupFeedbackWindow()
-
-
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        statusItem.button?.image = standbyImage
-        setupMenus()
-
-        guard let modelPath = Bundle.main.url(forResource: "ggml-base.en", withExtension: "bin", subdirectory: "models")?.path else {
-            logger.error("Could not find the model file")
-            return
-        }
-
-        Task {
-            do {
-                self.whisperContext = try WhisperContext.createContext(path: modelPath)
-                self.recorder = try await Recorder(whisperContext: self.whisperContext!)
-                logger.info("Whisper context and recorder created successfully")
-            } catch {
-                logger.error("Error creating Whisper context: \(error.localizedDescription)")
-            }
-        }
+    self.hotKey = HotKey(key: .h, modifiers: [.control, .shift])
+    hotKey?.keyDownHandler = { [weak self] in
+      self?.didTapRecording()
+    }
+    hotKey?.keyUpHandler = { [weak self] in
+      self?.didTapStandby()
     }
 
-    // Translated from espanso's injectString function
-    func insertStringAtCursor(_ string: String) {
-        let udelay = UInt32(1000)
+    setupFeedbackWindow()
 
-        DispatchQueue.main.async {
-            let buffer = Array(string.utf16)
+    statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    statusItem.button?.image = standbyImage
+    setupMenus()
 
-            // Because of a bug ( or undocumented limit ) of the CGEventKeyboardSetUnicodeString method
-            // the string gets truncated after 20 characters, so we need to send multiple events.
-            var i = 0
-            let chunkSize = 20;
-            while i < buffer.count {
-                let currentChunkSize = min(chunkSize, buffer.count - i)
-                let offsetBuffer = Array(buffer[i..<(i+currentChunkSize)])
+    guard
+      let modelPath = Bundle.main.url(
+        forResource: "ggml-base.en", withExtension: "bin", subdirectory: "models")?.path
+    else {
+      logger.error("Could not find the model file")
+      return
+    }
 
-                if let e = CGEvent(keyboardEventSource: nil, virtualKey: 0x31, keyDown: true) {
-                    e.keyboardSetUnicodeString(stringLength: currentChunkSize, unicodeString: offsetBuffer)
-                    e.post(tap: .cghidEventTap)
-                }
+    Task {
+      do {
+        self.whisperContext = try WhisperContext.createContext(path: modelPath)
+        self.recorder = try await Recorder(whisperContext: self.whisperContext!)
+        logger.info("Whisper context and recorder created successfully")
+      } catch {
+        logger.error("Error creating Whisper context: \(error.localizedDescription)")
+      }
+    }
+  }
 
-                usleep(udelay)
+  // Translated from espanso's injectString function
+  func insertStringAtCursor(_ string: String) {
+    let udelay = UInt32(1000)
 
-                i += currentChunkSize
-            }
+    DispatchQueue.main.async {
+      let buffer = Array(string.utf16)
+
+      // Because of a bug ( or undocumented limit ) of the CGEventKeyboardSetUnicodeString method
+      // the string gets truncated after 20 characters, so we need to send multiple events.
+      var i = 0
+      let chunkSize = 20
+      while i < buffer.count {
+        let currentChunkSize = min(chunkSize, buffer.count - i)
+        let offsetBuffer = Array(buffer[i..<(i + currentChunkSize)])
+
+        if let e = CGEvent(keyboardEventSource: nil, virtualKey: 0x31, keyDown: true) {
+          e.keyboardSetUnicodeString(stringLength: currentChunkSize, unicodeString: offsetBuffer)
+          e.post(tap: .cghidEventTap)
         }
+
+        usleep(udelay)
+
+        i += currentChunkSize
+      }
     }
+  }
 
-    func setupFeedbackWindow() {
-        feedbackWindow = NSWindow(contentRect: NSRect(x: 0, y: 0, width: 200, height: 40),
-                                  styleMask: [.borderless],
-                                  backing: .buffered,
-                                  defer: false)
+  func setupFeedbackWindow() {
+    feedbackWindow = NSWindow(
+      contentRect: NSRect(x: 0, y: 0, width: 200, height: 40),
+      styleMask: [.borderless],
+      backing: .buffered,
+      defer: false)
 
-        feedbackWindow?.level = .floating
-        feedbackWindow?.isOpaque = true
-        feedbackWindow?.hasShadow = true
-        feedbackWindow?.ignoresMouseEvents = true
+    feedbackWindow?.level = .floating
+    feedbackWindow?.isOpaque = true
+    feedbackWindow?.hasShadow = true
+    feedbackWindow?.ignoresMouseEvents = true
 
-        feedbackTextField = NSTextField(frame: NSRect(x: 0, y: 0, width: 200, height: 40))
-        feedbackTextField?.alignment = .center
-        feedbackTextField?.isBezeled = false
-        feedbackTextField?.drawsBackground = false
-        feedbackTextField?.isEditable = false
-        feedbackTextField?.isSelectable = false
+    feedbackTextField = NSTextField(frame: NSRect(x: 0, y: 0, width: 200, height: 40))
+    feedbackTextField?.alignment = .center
+    feedbackTextField?.isBezeled = false
+    feedbackTextField?.drawsBackground = false
+    feedbackTextField?.isEditable = false
+    feedbackTextField?.isSelectable = false
 
-        feedbackWindow?.contentView?.addSubview(feedbackTextField!)
+    feedbackWindow?.contentView?.addSubview(feedbackTextField!)
+  }
+
+  func showFeedback(_ text: String?) {
+    DispatchQueue.main.async { [weak self] in
+      guard let self = self else { return }
+      if let text = text {
+        let mouseLocation = NSEvent.mouseLocation
+        self.feedbackWindow?.setFrameOrigin(NSPoint(x: mouseLocation.x, y: mouseLocation.y))
+
+        self.feedbackTextField?.stringValue = text
+        self.feedbackWindow?.makeKeyAndOrderFront(nil)
+      } else {
+        self.feedbackWindow?.orderOut(nil)
+      }
     }
+  }
 
-    func showFeedback(_ text: String?) {
-        DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return }
-            if let text = text {
-                let mouseLocation = NSEvent.mouseLocation
-                self.feedbackWindow?.setFrameOrigin(NSPoint(x: mouseLocation.x, y: mouseLocation.y))
+  //TODO: make this menu a microphone input selector?
+  func setupMenus() {
+    let menu = NSMenu()
 
-                self.feedbackTextField?.stringValue = text
-                self.feedbackWindow?.makeKeyAndOrderFront(nil)
-            } else {
-                self.feedbackWindow?.orderOut(nil)
-            }
-        }
+    let about = NSMenuItem(title: "About", action: #selector(openAbout), keyEquivalent: "")
+    menu.addItem(about)
+
+    menu.addItem(NSMenuItem.separator())
+
+    let standby = NSMenuItem(title: "Standby", action: #selector(didTapStandby), keyEquivalent: "1")
+    menu.addItem(standby)
+
+    let recording = NSMenuItem(
+      title: "Recording", action: #selector(didTapRecording), keyEquivalent: "2")
+    menu.addItem(recording)
+
+    menu.addItem(NSMenuItem.separator())
+
+    menu.addItem(
+      NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+
+    statusItem.menu = menu
+  }
+
+  @objc func openAbout() {
+    if let url = URL(string: "https://www.github.com/lynaghk") {
+      NSWorkspace.shared.open(url)
     }
+  }
 
+  @objc func didTapStandby() {
+    logger.debug("didTapStandby")
+    showFeedback("transcribing...")
+    statusItem.button?.image = standbyImage
 
-    //TODO: make this menu a microphone input selector?
-    func setupMenus() {
-        let menu = NSMenu()
-
-        let about = NSMenuItem(title: "About", action: #selector(openAbout), keyEquivalent: "")
-        menu.addItem(about)
-
-        menu.addItem(NSMenuItem.separator())
-
-        let standby = NSMenuItem(title: "Standby", action: #selector(didTapStandby) , keyEquivalent: "1")
-        menu.addItem(standby)
-
-        let recording = NSMenuItem(title: "Recording", action: #selector(didTapRecording) , keyEquivalent: "2")
-        menu.addItem(recording)
-
-        menu.addItem(NSMenuItem.separator())
-
-        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-
-        statusItem.menu = menu
+    Task {
+      await recorder!.stopRecording()
+      if await recorder!.recordedDurationSeconds() > MinimumTranscriptionDuration {
+        lastTranscript = await recorder!.transcribe()
+        self.insertStringAtCursor(lastTranscript)
+      } else {
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.setString(lastTranscript, forType: .string)
+      }
+      showFeedback(nil)
     }
+  }
 
-    @objc func openAbout() {
-        if let url = URL(string: "https://www.github.com/lynaghk") {
-            NSWorkspace.shared.open(url)
-        }
+  @objc func didTapRecording() {
+    logger.debug("didTapRecording")
+    showFeedback("recording")
+    statusItem.button?.image = recordingImage
+
+    Task {
+      do {
+        try await recorder?.startRecording()
+      } catch {
+        logger.error("Error starting recording: \(error.localizedDescription)")
+      }
     }
-
-    @objc func didTapStandby() {
-        logger.debug("didTapStandby")
-        showFeedback("transcribing...")
-        statusItem.button?.image = standbyImage
-
-        Task {
-            await recorder!.stopRecording()
-            if await recorder!.recordedDurationSeconds() > MinimumTranscriptionDuration {
-                lastTranscript = await recorder!.transcribe()
-                self.insertStringAtCursor(lastTranscript)
-            } else {
-                NSPasteboard.general.clearContents()
-                NSPasteboard.general.setString(lastTranscript, forType: .string)
-            }
-            showFeedback(nil)
-        }
-    }
-
-    @objc func didTapRecording() {
-        logger.debug("didTapRecording")
-        showFeedback("recording")
-        statusItem.button?.image = recordingImage
-
-        Task {
-            do {
-                try await recorder?.startRecording()
-            } catch {
-                logger.error("Error starting recording: \(error.localizedDescription)")
-            }
-        }
-    }
+  }
 }
--- whispertron/Recorder.swift
+++ whispertron/Recorder.swift
@@ -1,141 +1,150 @@
-import Foundation
-import AVFoundation
-import whisper
 import AVFAudio
+import AVFoundation
 import CoreAudio
+import Foundation
 import os
+import whisper
 
 func requestMicrophonePermission() async -> Bool {
-    await withCheckedContinuation { continuation in
-        AVCaptureDevice.requestAccess(for: .audio) { granted in
-            continuation.resume(returning: granted)
-        }
+  await withCheckedContinuation { continuation in
+    AVCaptureDevice.requestAccess(for: .audio) { granted in
+      continuation.resume(returning: granted)
     }
+  }
 }
 
 actor Recorder {
-    private var audioEngine: AVAudioEngine
-    private var whisperContext: WhisperContext
-    private var inputNode: AVAudioInputNode
-    private var bufferSize: AVAudioFrameCount = 1024
-    private var whisperSampleRate = Double(WHISPER_SAMPLE_RATE)
+  private var audioEngine: AVAudioEngine
+  private var whisperContext: WhisperContext
+  private var inputNode: AVAudioInputNode
+  private var bufferSize: AVAudioFrameCount = 1024
+  private var whisperSampleRate = Double(WHISPER_SAMPLE_RATE)
 
-    private var audioBuffer: [Float] = []
-    private var inputFormat: AVAudioFormat
-    private var converter: AVAudioConverter?
+  private var audioBuffer: [Float] = []
+  private var inputFormat: AVAudioFormat
+  private var converter: AVAudioConverter?
 
-    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "Recorder")
+  private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "Recorder")
 
+  enum RecorderError: Error {
+    case audioEngineError(String)
+  }
 
+  init(whisperContext: WhisperContext) async throws {
+    self.whisperContext = whisperContext
+    self.audioEngine = AVAudioEngine()
+    self.inputNode = audioEngine.inputNode
 
-    enum RecorderError: Error {
-        case audioEngineError(String)
+    let granted = await requestMicrophonePermission()
+    if granted {
+      self.logger.info("Microphone permission granted")
+    } else {
+      self.logger.error("Microphone permission not granted")
+      throw RecorderError.audioEngineError("Microphone permission denied")
     }
 
-    init(whisperContext: WhisperContext) async throws {
-        self.whisperContext = whisperContext
-        self.audioEngine = AVAudioEngine()
-        self.inputNode = audioEngine.inputNode
+    self.inputFormat = self.inputNode.inputFormat(forBus: 0)
 
-        let granted = await requestMicrophonePermission()
-        if granted {
-            self.logger.info("Microphone permission granted")
-        } else {
-            self.logger.error("Microphone permission not granted")
-            throw RecorderError.audioEngineError("Microphone permission denied")
+    // let output = audioEngine.mainMixerNode
+    // audioEngine.connect(inputNode, to: output, format: inputNode.inputFormat(forBus: 0))
+    // audioEngine.prepare()
+    // do {
+    //     try audioEngine.start()
+    //     print("Audio echo started")
+    // } catch {
+    //     print("Could not start audio engine: \(error.localizedDescription)")
+
+    // }
+
+    let inputFormat = AVAudioFormat(
+      commonFormat: .pcmFormatFloat32, sampleRate: self.inputFormat.sampleRate, channels: 1,
+      interleaved: false)!
+    let outputFormat = AVAudioFormat(
+      commonFormat: .pcmFormatFloat32, sampleRate: whisperSampleRate, channels: 1,
+      interleaved: false)!
+    self.converter = AVAudioConverter(from: inputFormat, to: outputFormat)
+  }
+
+  func startRecording() throws {
+    let sampleRate = inputFormat.sampleRate
+    let format = AVAudioFormat(
+      commonFormat: .pcmFormatFloat32, sampleRate: sampleRate, channels: 1, interleaved: false)
+
+    inputNode.installTap(onBus: 0, bufferSize: bufferSize, format: format) {
+      [weak self] buffer, _ in
+      guard let self = self else { return }
+      let channelData = buffer.floatChannelData?[0]
+      let frameLength = Int(buffer.frameLength)
+
+      if let channelData = channelData {
+        let samples = Array(UnsafeBufferPointer(start: channelData, count: frameLength))
+        Task {
+          await self.processAudio(samples: samples)
         }
-
-        self.inputFormat = self.inputNode.inputFormat(forBus: 0)
-
-
-        // let output = audioEngine.mainMixerNode
-        // audioEngine.connect(inputNode, to: output, format: inputNode.inputFormat(forBus: 0))
-        // audioEngine.prepare()
-        // do {
-        //     try audioEngine.start()
-        //     print("Audio echo started")
-        // } catch {
-        //     print("Could not start audio engine: \(error.localizedDescription)")
-
-        // }
-
-
-        let inputFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: self.inputFormat.sampleRate, channels: 1, interleaved: false)!
-        let outputFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: whisperSampleRate, channels: 1, interleaved: false)!
-        self.converter = AVAudioConverter(from: inputFormat, to: outputFormat)
+      }
     }
 
-    func startRecording() throws {
-        let sampleRate = inputFormat.sampleRate
-        let format = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: sampleRate, channels: 1, interleaved: false)
+    audioEngine.prepare()
 
-        inputNode.installTap(onBus: 0, bufferSize: bufferSize, format: format) { [weak self] buffer, _ in
-            guard let self = self else { return }
-            let channelData = buffer.floatChannelData?[0]
-            let frameLength = Int(buffer.frameLength)
-
-            if let channelData = channelData {
-                let samples = Array(UnsafeBufferPointer(start: channelData, count: frameLength))
-                Task {
-                    await self.processAudio(samples: samples)
-                }
-            }
-        }
-
-        audioEngine.prepare()
-
-        do {
-            try audioEngine.start()
-        } catch {
-            throw RecorderError.audioEngineError("Could not start audio engine: \(error.localizedDescription)")
-        }
-
-        logger.info("Started recording")
+    do {
+      try audioEngine.start()
+    } catch {
+      throw RecorderError.audioEngineError(
+        "Could not start audio engine: \(error.localizedDescription)")
     }
 
-    private func processAudio(samples: [Float]) async {
-        if let downsampledChunk = downsample(samples) {
-            audioBuffer.append(contentsOf: downsampledChunk)
-        }
+    logger.info("Started recording")
+  }
+
+  private func processAudio(samples: [Float]) async {
+    if let downsampledChunk = downsample(samples) {
+      audioBuffer.append(contentsOf: downsampledChunk)
+    }
+  }
+
+  func stopRecording() {
+    audioEngine.stop()
+    inputNode.removeTap(onBus: 0)
+    logger.info("Stopped recording, transcribing...")
+  }
+
+  func recordedDurationSeconds() -> Double {
+    Double(audioBuffer.count) / whisperSampleRate
+  }
+
+  private func downsample(_ samples: [Float]) -> [Float]? {
+    guard let converter = converter else { return nil }
+
+    let inputBuffer = AVAudioPCMBuffer(
+      pcmFormat: converter.inputFormat, frameCapacity: AVAudioFrameCount(samples.count))!
+    let outputBuffer = AVAudioPCMBuffer(
+      pcmFormat: converter.outputFormat,
+      frameCapacity: AVAudioFrameCount(
+        Double(samples.count) * converter.outputFormat.sampleRate / converter.inputFormat.sampleRate
+      ))!
+
+    inputBuffer.frameLength = inputBuffer.frameCapacity
+    memcpy(inputBuffer.floatChannelData?[0], samples, samples.count * MemoryLayout<Float>.size)
+
+    var error: NSError?
+    let status = converter.convert(to: outputBuffer, error: &error) { inNumPackets, outStatus in
+      outStatus.pointee = .haveData
+      return inputBuffer
     }
 
-    func stopRecording() {
-        audioEngine.stop()
-        inputNode.removeTap(onBus: 0)
-        logger.info("Stopped recording, transcribing...")
+    guard status != .error, error == nil else {
+      logger.error("Conversion failed: \(error?.localizedDescription ?? "Unknown error")")
+      return nil
     }
 
-    func recordedDurationSeconds() -> Double {
-        Double(audioBuffer.count) / whisperSampleRate
-    }
+    let downsampledData = UnsafeBufferPointer(
+      start: outputBuffer.floatChannelData?[0], count: Int(outputBuffer.frameLength))
+    return Array(downsampledData)
+  }
 
-    private func downsample(_ samples: [Float]) -> [Float]? {
-        guard let converter = converter else { return nil }
-
-        let inputBuffer = AVAudioPCMBuffer(pcmFormat: converter.inputFormat, frameCapacity: AVAudioFrameCount(samples.count))!
-        let outputBuffer = AVAudioPCMBuffer(pcmFormat: converter.outputFormat, frameCapacity: AVAudioFrameCount(Double(samples.count) * converter.outputFormat.sampleRate / converter.inputFormat.sampleRate))!
-
-        inputBuffer.frameLength = inputBuffer.frameCapacity
-        memcpy(inputBuffer.floatChannelData?[0], samples, samples.count * MemoryLayout<Float>.size)
-
-        var error: NSError?
-        let status = converter.convert(to: outputBuffer, error: &error) { inNumPackets, outStatus in
-            outStatus.pointee = .haveData
-            return inputBuffer
-        }
-
-        guard status != .error, error == nil else {
-            logger.error("Conversion failed: \(error?.localizedDescription ?? "Unknown error")")
-            return nil
-        }
-
-        let downsampledData = UnsafeBufferPointer(start: outputBuffer.floatChannelData?[0], count: Int(outputBuffer.frameLength))
-        return Array(downsampledData)
-    }
-
-    func transcribe() async -> String {
-        await whisperContext.fullTranscribe(samples: audioBuffer)
-        audioBuffer.removeAll()
-        return await whisperContext.getTranscription()
-    }
+  func transcribe() async -> String {
+    await whisperContext.fullTranscribe(samples: audioBuffer)
+    audioBuffer.removeAll()
+    return await whisperContext.getTranscription()
+  }
 }
