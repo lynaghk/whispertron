ee119e6cc3fc17b35d7c77d8c58460184bd056d7
Scott Becker <scott@olioapps.com>
2025-06-17 13:39:16 -0700

    Use curl for install

--- build.sh
+++ build.sh
@@ -13,14 +13,14 @@ FILE_PATH="whispertron/models/model.bin"
 if [ -f "$FILE_PATH" ]; then
     echo "Whisper model already downloaded."
 else
-    wget -O "$FILE_PATH" "https://huggingface.co/ggerganov/whisper.cpp/resolve/main/$MODEL.bin?download=true"
+    curl -L -o "$FILE_PATH" "https://huggingface.co/ggerganov/whisper.cpp/resolve/main/$MODEL.bin?download=true"
 fi
 
 FILE_PATH="whisper-v1.7.5-xcframework.zip"
 if [ -f "$FILE_PATH" ]; then
     echo "Whisper framework already downloaded."
 else
-    wget -O "$FILE_PATH" "https://github.com/ggml-org/whisper.cpp/releases/download/v1.7.5/whisper-v1.7.5-xcframework.zip"
+    curl -L -o "$FILE_PATH" "https://github.com/ggml-org/whisper.cpp/releases/download/v1.7.5/whisper-v1.7.5-xcframework.zip"
     unzip -d whisper_xcframework whisper-v1.7.5-xcframework.zip
 fi
 
